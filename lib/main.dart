21830f411f73a95bf8b187644ceff7eafd0d6648
erencelik <erenmemo05@gmail.com>
2022-12-30 12:37:27 +0300

    addes question stateless widget and imported it to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/question.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,14 +11,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var questionNumber = 0;
+  var _questionNumber = 0;
   var questions = [
     'What\'s your favorite color?',
     'What\'s your favorite animal'
   ];
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionNumber += 1;
+      if (_questionNumber >= 1) {
+        return;
+      }
+      _questionNumber += 1;
     });
 
     print('Answer Choosen');
@@ -32,11 +36,11 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionNumber]),
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 2')),
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 3')),
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 4')),
+            Question(questions[_questionNumber]),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 1')),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 2')),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 3')),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 4')),
           ],
         ),
       ),
